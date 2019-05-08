27aff7ef20d8b25888e2cc8b5190f702305e6a38
Zheyong Fan <brucenju@gmail.com>
2019-05-08 13:03:05 +0300

    safely open file

--- src/chapter-6/neighbor1.cpp
+++ src/chapter-6/neighbor1.cpp
@@ -28,12 +28,13 @@ void read_xy(int N, double *x, double *y)
 {
     double Lx, Ly;
     FILE *fid = fopen("xy.txt", "r");
+    if (NULL == fid) { printf("Cannot open xy.in"); exit(1); }
     fscanf(fid, "%d", &N);
     fscanf(fid, "%lf%lf", &Lx, &Ly);
     for (int n = 0; n < N; ++n)
     {
         int count = fscanf(fid, "%lf%lf", &x[n], &y[n]);
-        if (count != 2) { printf("Error for reading xy.in"); exit(1);}
+        if (count != 2) { printf("Error for reading xy.in"); exit(1); }
     }
     fclose(fid);
 }
