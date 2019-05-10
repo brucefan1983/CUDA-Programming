ae6c673223821456cdd9f9eea1229c46daa83ad2
Zheyong Fan <brucenju@gmail.com>
2019-05-10 21:24:34 +0300

    Update length1.cpp

--- src/chapter-7/length1.cpp
+++ src/chapter-7/length1.cpp
@@ -1,6 +1,7 @@
 #include <stdlib.h> // malloc() and free()
 #include <stdio.h> // printf()
 #include <math.h> // sqrt()
+#include <time.h> // clock_t, clock(), and CLOCKS_PER_SEC
 double get_length(double *x, int N);
 
 int main(void)
@@ -12,7 +13,15 @@ int main(void)
     {
         x[n] = 1.0;
     }
-    double length = get_length(x, N);
+    clock_t time_begin = clock();
+    double length = 0.0;
+    for (int n = 0; n < 100000; ++n)
+    {
+        length = get_length(x, N);
+    }
+    clock_t time_finish = clock();
+    double time_used = (time_finish - time_begin) / double(CLOCKS_PER_SEC);
+    printf("Time used for 100000 host function callings = %f s.\n", time_used);
     printf("length = %g.\n", length);
     free(x);
     return 0;
