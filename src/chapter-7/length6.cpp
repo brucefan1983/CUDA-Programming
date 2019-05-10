f421bdf74eeaf469df596f4cd08504cdbc1380a6
Zheyong Fan <brucenju@gmail.com>
2019-05-10 16:02:09 +0300

    N=10^8

---
+++ src/chapter-7/length6.cpp
@@ -0,0 +1,36 @@
+#include <stdlib.h> // malloc() and free()
+#include <stdio.h> // printf()
+#include <math.h> // sqrt()
+#include <time.h> // clock_t, clock(), and CLOCKS_PER_SEC
+double get_length(double *x, int N);
+
+int main(void)
+{
+    int N = 100000000;
+    int M = sizeof(double) * N;
+    double *x = (double *) malloc(M);
+    for (int n = 0; n < N; ++n)
+    {
+        x[n] = 1.0;
+    }
+    clock_t time_begin = clock();
+    double length = get_length(x, N);
+    clock_t time_finish = clock();
+    double time_used = (time_finish - time_begin) / double(CLOCKS_PER_SEC);
+    printf("Time used for host function calling = %f s.\n", time_used);
+    printf("length = %g.\n", length);
+    free(x);
+    return 0;
+}
+
+double get_length(double *x, int N)
+{
+    double length = 0.0;
+    for (int n = 0; n < N; ++n)
+    {
+        length += x[n] * x[n];
+    }
+    length = sqrt(length);
+    return length;
+}
+
