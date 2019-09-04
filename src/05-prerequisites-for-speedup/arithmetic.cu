a000d3632ad020c6da02afb09d3380d04ac65861
Zheyong Fan <brucenju@gmail.com>
2019-09-04 15:59:51 +0300

    double to real

--- src/05-prerequisites-for-speedup/arithmetic.cu
+++ src/05-prerequisites-for-speedup/arithmetic.cu
@@ -3,21 +3,29 @@
 #include <stdio.h>
 #include <time.h>
 #define EPSILON 1.0e-14
-void __global__ arithmetic(double *x, double *y, int N);
-void check(double *z, int N);
+
+#ifdef USE_DP
+    typedef double real;
+    #define EPSILON 1.0e-14
+#else
+    typedef float real;
+    #define EPSILON 1.0e-6
+#endif
+void __global__ arithmetic(real *x, real *y, int N);
+void check(real *z, int N);
 
 int main(int argc, char **argv)
 {
     int N = atoi(argv[1]);
     int block_size = atoi(argv[2]);
-    int M = sizeof(double) * N;
-    double *x = (double*) malloc(M);
-    double *y = (double*) malloc(M);
+    int M = sizeof(real) * N;
+    real *x = (real*) malloc(M);
+    real *y = (real*) malloc(M);
     for (int n = 0; n < N; ++n)
     {
         x[n] = 2.0; y[n] = 0.0;
     }
-    double *g_x, *g_y;
+    real *g_x, *g_y;
     CHECK(cudaMalloc((void **)&g_x, M))
     CHECK(cudaMalloc((void **)&g_y, M))
     CHECK(cudaMemcpy(g_x, x, M, cudaMemcpyHostToDevice))
@@ -35,20 +43,20 @@ int main(int argc, char **argv)
     return 0;
 }
 
-void __global__ arithmetic(double *x, double *y, int N)
+void __global__ arithmetic(real *x, real *y, int N)
 {
     int n = blockDim.x * blockIdx.x + threadIdx.x;
     if (n < N) 
     {
-        double x1 = x[n];
-        double x30 = pow(x1, 30.0);
-        double sin_x = sin(x30);
-        double cos_x = cos(x30);
+        real x1 = x[n];
+        real x30 = pow(x1, 30.0);
+        real sin_x = sin(x30);
+        real cos_x = cos(x30);
         y[n] = sin_x * sin_x + cos_x * cos_x;
     }
 }
 
-void check(double *y, int N)
+void check(real *y, int N)
 {
     int has_error = 0;
     for (int n = 0; n < N; ++n)
