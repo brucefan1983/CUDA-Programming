8b470c0fbdf73c13cc3d692a0b2d477cf7c15f4e
Zheyong Fan <brucenju@gmail.com>
2019-09-04 15:55:16 +0300

    pow to arithmetic

--- src/05-prerequisites-for-speedup/arithmetic.cpp
+++ src/05-prerequisites-for-speedup/arithmetic.cpp
@@ -3,8 +3,8 @@
 #include <stdio.h>
 #include <time.h>
 #define EPSILON 1.0e-14
-void power(double *x, double *y, double *z, int N);
-void check(double *z, int N);
+void arithmetic(double *x, double *y, int N);
+void check(double *y, int N);
 
 int main(void)
 {
@@ -12,38 +12,42 @@ int main(void)
     int M = sizeof(double) * N;
     double *x = (double*) malloc(M);
     double *y = (double*) malloc(M);
-    double *z = (double*) malloc(M);
     for (int n = 0; n < N; ++n)
     {
-        x[n] = 1.0; y[n] = 2.0; z[n] = 0.0;
+        x[n] = 100.0;
+        y[n] = 0.0;
     }
-
     clock_t time_begin = clock();
-    power(x, y, z, N);
+    arithmetic(x, y, N);
     clock_t time_finish = clock();
     double time_used = (time_finish - time_begin)
-        / double(CLOCKS_PER_SEC);
+                     / double(CLOCKS_PER_SEC);
     printf("Time used for host function = %g s.\n", time_used);
 
-    check(z, N);
-    free(x); free(y); free(z);
+    check(y, N);
+
+    free(x); free(y);
     return 0;
 }
 
-void power(double *x, double *y, double *z, int N)
+void arithmetic(double *x, double *y, int N)
 {
     for (int n = 0; n < N; ++n)
     {
-        z[n] = pow(x[n], y[n]);
+        double x1 = x[n];
+        double x30 = pow(x1, 30.0);
+        double sin_x = sin(x30);
+        double cos_x = cos(x30);
+        y[n] = sin_x * sin_x + cos_x * cos_x;
     }
 }
 
-void check(double *z, int N)
+void check(double *y, int N)
 {
     int has_error = 0;
     for (int n = 0; n < N; ++n)
     {
-        has_error += (fabs(z[n] - 1.0) > EPSILON);
+        has_error += (fabs(y[n] - 1.0) > EPSILON);
     }
     printf("%s\n", has_error ? "Has errors" : "No errors");
 }
--- src/05-prerequisites-for-speedup/arithmetic.cu
+++ src/05-prerequisites-for-speedup/arithmetic.cu
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <time.h>
 #define EPSILON 1.0e-14
-void __global__ power(double *x, double *y, double *z, int N);
+void __global__ arithmetic(double *x, double *y, int N);
 void check(double *z, int N);
 
 int main(int argc, char **argv)
@@ -13,23 +13,20 @@ int main(int argc, char **argv)
     int M = sizeof(double) * N;
     double *x = (double*) malloc(M);
     double *y = (double*) malloc(M);
-    double *z = (double*) malloc(M);
     for (int n = 0; n < N; ++n)
     {
-        x[n] = 1.0; y[n] = 2.0; z[n] = 0.0;
+        x[n] = 2.0; y[n] = 0.0;
     }
-    double *g_x, *g_y, *g_z;
+    double *g_x, *g_y;
     CHECK(cudaMalloc((void **)&g_x, M))
     CHECK(cudaMalloc((void **)&g_y, M))
-    CHECK(cudaMalloc((void **)&g_z, M))
     CHECK(cudaMemcpy(g_x, x, M, cudaMemcpyHostToDevice))
-    CHECK(cudaMemcpy(g_y, y, M, cudaMemcpyHostToDevice))
 
     int grid_size = (N - 1) / block_size + 1;
-    power<<<grid_size, block_size>>>(g_x, g_y, g_z, N);
+    arithmetic<<<grid_size, block_size>>>(g_x, g_y, N);
    
-    cudaMemcpy(z, g_z, M, cudaMemcpyDeviceToHost);
-    check(z, N);
+    cudaMemcpy(y, g_yy, M, cudaMemcpyDeviceToHost);
+    check(, N);
 
     free(x); free(y); free(z);
     CHECK(cudaFree(g_x))
@@ -38,18 +35,25 @@ int main(int argc, char **argv)
     return 0;
 }
 
-void __global__ power(double *x, double *y, double *z, int N)
+void __global__ arithmetic(double *x, double *y, int N)
 {
     int n = blockDim.x * blockIdx.x + threadIdx.x;
-    if (n < N) { z[n] = pow(x[n], y[n]); }
+    if (n < N) 
+    {
+        double x1 = x[n];
+        double x30 = pow(x1, 30.0);
+        double sin_x = sin(x30);
+        double cos_x = cos(x30);
+        y[n] = sin_x * sin_x + cos_x * cos_x;
+    }
 }
 
-void check(double *z, int N)
+void check(double *y, int N)
 {
     int has_error = 0;
     for (int n = 0; n < N; ++n)
     {
-        has_error += (fabs(z[n] - 1.0) > EPSILON);
+        has_error += (fabs(y[n] - 1.0) > EPSILON);
     }
     printf("%s\n", has_error ? "Has errors" : "No errors");
 }
