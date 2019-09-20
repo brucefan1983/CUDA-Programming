87fb9e30eca34964697099151bf26abbe35f8357
Zheyong Fan <brucenju@gmail.com>
2019-09-20 22:36:50 +0300

    add an example of static global memory

---
+++ src/06-memory/global.cu
@@ -0,0 +1,16 @@
+#include <stdio.h>
+__device__ int x = 1;
+__device__ int y[2];
+
+void __global__ my_kernel(void)
+{
+    y[0] = x + 1;
+    y[1] = x + 2;
+    printf("x = %d, y[0] = %d, y[1] = %d.\n", x, y[0], y[1]);
+}
+int main(void)
+{
+    my_kernel<<<1, 1>>>();
+    cudaDeviceReset();
+    return 0;
+}
