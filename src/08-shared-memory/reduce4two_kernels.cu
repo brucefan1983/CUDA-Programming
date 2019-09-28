88d314c49f8ab9b26cd27fe01fe020864fd38552
Zheyong Fan <brucenju@gmail.com>
2019-09-28 20:16:49 +0300

    add one program

--- src/08-shared-memory/reduce4two_kernels.cu
+++ src/08-shared-memory/reduce4two_kernels.cu
@@ -27,7 +27,7 @@ int main(int argc, char **argv)
     return 0;
 }
 
-void __global__ reduce(real *g_x, real *g_y, int N)
+void __global__ reduce_1(real *g_x, real *g_y, int N)
 {
     int tid = threadIdx.x;
     int bid = blockIdx.x;
@@ -52,25 +52,50 @@ void __global__ reduce(real *g_x, real *g_y, int N)
     if (tid == 0) { g_y[bid] = s_y[0]; }
 }
 
+void __global__ reduce_2 
+(real *g_x, real *g_sum, int N, int number_of_rounds)
+{
+    int tid = threadIdx.x;
+    __shared__ real s_sum[1024];
+    real tmp_sum = 0.0;
+    for (int round = 0; round < number_of_rounds; ++round)
+    {
+        int n = tid + round * 1024;
+        if (n < N) { tmp_sum += g_x[n]; }
+    }
+    s_sum[tid] = tmp_sum;
+    __syncthreads();
+
+    for (int offset = blockDim.x >> 1; offset > 0; offset >>= 1)
+    {
+        if (tid < offset) { s_sum[tid] += s_sum[tid + offset]; }
+        __syncthreads();
+    }
+    
+    if (tid == 0) { g_sum[0] = s_sum[0]; }
+}
+
 real reduce(real *x, int N)
 {
     const int block_size = 128;
     int grid_size = (N - 1) / (block_size * NUM_ROUNDS) + 1;
+    int number_of_rounds = (grid_size - 1) / 1024 + 1;
 
-    real *y;
+    real *y, *sum;
     CHECK(cudaMalloc(&y, sizeof(real) * grid_size))
+    CHECK(cudaMalloc(&sum, sizeof(real)))
 
-    reduce<<<grid_size, block_size>>>(x, y, N);
+    reduce_1<<<grid_size, block_size>>>(x, y, N);
+    reduce_2<<<1, 1024>>>(y, sum, grid_size, number_of_rounds);
 
-    real *h_y = (real *)malloc(sizeof(real) * grid_size);
-    CHECK(cudaMemcpy(h_y, y, sizeof(real) * grid_size, 
+    real *h_sum = (real *)malloc(sizeof(real));
+    CHECK(cudaMemcpy(h_sum, sum, sizeof(real), 
         cudaMemcpyDeviceToHost))
+    real result = h_sum[0];
 
-    real result = 0.0;
-    for (int n = 0; n < grid_size; ++n) { result += h_y[n]; }
-
-    free(h_y);
+    free(h_sum);
     CHECK(cudaFree(y))
+    CHECK(cudaFree(sum))
     return result;
 }
 
