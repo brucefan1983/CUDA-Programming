e8e07b7fbe18506d758b49527aff2369c13151f4
Zheyong Fan <brucenju@gmail.com>
2019-10-29 14:25:07 +0200

    faster

--- src/09-atomic/reduce5atomic.cu
+++ src/09-atomic/reduce5atomic.cu
@@ -1,77 +1,85 @@
 #include "error.cuh"
 #include <stdio.h>
+
 #ifdef USE_DP
     typedef double real;
 #else
     typedef float real;
 #endif
-real reduce(real *x, int N, int K);
 
-int main(int argc, char **argv)
+const int NUM_ROUNDS = 10;
+
+real reduce(const real *x, const int N);
+
+int main(void)
 {
-    int K = atoi(argv[1]);
-    int N = 100000000;
-    int M = sizeof(real) * N;
+    const int N = 100000000;
+    const int M = sizeof(real) * N;
     real *h_x = (real *)malloc(M);
-    for (int n = 0; n < N; ++n) { h_x[n] = 1.0; }
+    for (int n = 0; n < N; ++n)
+    {
+        h_x[n] = 1.0;
+    }
     real *x;
     CHECK(cudaMalloc(&x, M))
     CHECK(cudaMemcpy(x, h_x, M, cudaMemcpyHostToDevice))
 
-    real sum = reduce(x, N, K);
-    printf("sum = %g.\n", sum);
+    real sum = reduce(x, N);
+    printf("sum = %f.\n", sum);
 
     free(h_x);
     CHECK(cudaFree(x))
     return 0;
 }
 
-void __global__ reduce_1
-(real *g_x, real *g_y, int N, int number_of_rounds)
+void __global__ reduce(const real *g_x, real *g_y, const int N)
 {
     int tid = threadIdx.x;
     int bid = blockIdx.x;
-    __shared__ real s_y[128];
+    extern __shared__ real s_y[];
 
     real y = 0.0;
-    int offset = tid + bid * blockDim.x * number_of_rounds;
-    for (int round = 0; round < number_of_rounds; ++round)
+    int offset = tid + bid * blockDim.x * NUM_ROUNDS;
+    for (int round = 0; round < NUM_ROUNDS; ++round)
     {
         int n = round * blockDim.x + offset;
-        if (n < N) { y += g_x[n]; }
+        if (n < N)
+        {
+            y += g_x[n];
+        }
     }
     s_y[tid] = y;
-    __syncthreads();
-    
+
     for (int offset = blockDim.x >> 1; offset > 0; offset >>= 1)
     {
-        if (tid < offset) { s_y[tid] += s_y[tid + offset]; }
         __syncthreads();
+        if (tid < offset)
+        {
+            s_y[tid] += s_y[tid + offset];
+        }
     }
 
     if (tid == 0) { atomicAdd(g_y, s_y[0]); }
 }
 
-real reduce(real *x, int N, int K)
+real reduce(const real *x, const int N)
 {
-    int block_size = 128;
-    int grid_size = (N - 1) / (block_size * K) + 1;
+    const int block_size = 128;
+    const int grid_size = (N - 1) / (block_size * NUM_ROUNDS) + 1;
 
-    real *h_sum = (real *)malloc(sizeof(real));
-    h_sum[0] = 0.0;
-    real *sum;
-    CHECK(cudaMalloc(&sum, sizeof(real)))
-    CHECK(cudaMemcpy(sum, h_sum, sizeof(real), 
-        cudaMemcpyHostToDevice))
+    real *y;
+    CHECK(cudaMalloc(&y, sizeof(real)))
 
-    reduce_1<<<grid_size, block_size>>>(x, sum, N, K);
+    reduce<<<grid_size, block_size, sizeof(real)*block_size>>>
+    (x, y, N);
 
-    CHECK(cudaMemcpy(h_sum, sum, sizeof(real), 
-        cudaMemcpyDeviceToHost))
-    real result = h_sum[0];
+    real *h_y = (real *)malloc(sizeof(real));
+    CHECK(cudaMemcpy(h_y, y, sizeof(real), cudaMemcpyDeviceToHost))
 
-    free(h_sum);
-    CHECK(cudaFree(sum))
+    real result = h_y[0];
+
+    free(h_y);
+    CHECK(cudaFree(y))
     return result;
 }
 
