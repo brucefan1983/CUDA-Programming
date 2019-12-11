353327d572a63650f5dd7bd930b65d76dc4c2dd4
Zheyong Fan <brucenju@gmail.com>
2019-12-11 20:28:48 +0800

    cudaDeviceSynchronize

--- src/06-memory/global.cu
+++ src/06-memory/global.cu
@@ -9,10 +9,11 @@ void __global__ my_kernel(void)
     y[1] = x + 2;
     printf("x = %d, y[0] = %d, y[1] = %d.\n", x, y[0], y[1]);
 }
+
 int main(void)
 {
     my_kernel<<<1, 1>>>();
-    CHECK(cudaDeviceReset());
+    CHECK(cudaDeviceSynchronize());
     return 0;
 }
 
