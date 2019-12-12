3d6035693d990afdbd69d2825a2451a01510d4d1
Zheyong Fan <brucenju@gmail.com>
2019-12-12 17:33:50 +0800

    simplify the codes
    and use Event for timing

---
+++ src/08-shared-memory/reduce_cpu.cu
@@ -0,0 +1,77 @@
+#include "error.cuh"
+#include <stdio.h>
+
+#ifdef USE_DP
+    typedef double real;
+#else
+    typedef float real;
+#endif
+
+const int NUM_REPEATS = 10;
+void timing(const real *x, const int N);
+real reduce(const real *x, const int N);
+
+int main(void)
+{
+    const int N = 100000000;
+    const int M = sizeof(real) * N;
+    real *x = (real *) malloc(M);
+    for (int n = 0; n < N; ++n)
+    {
+        x[n] = 1.23;
+    }
+
+    timing(x, N);
+
+    free(x);
+    return 0;
+}
+
+void timing(const real *x, const int N)
+{
+    real sum = 0;
+    float t_sum = 0;
+    float t2_sum = 0;
+    for (int repeat = 0; repeat <= NUM_REPEATS; ++repeat)
+    {
+        cudaEvent_t start, stop;
+        CHECK(cudaEventCreate(&start));
+        CHECK(cudaEventCreate(&stop));
+        CHECK(cudaEventRecord(start));
+
+        sum = reduce(x, N);
+
+        CHECK(cudaEventRecord(stop));
+        CHECK(cudaEventSynchronize(stop));
+        float elapsed_time;
+        CHECK(cudaEventElapsedTime(&elapsed_time, start, stop));
+        printf("Time = %g ms.\n", elapsed_time);
+
+        if (repeat > 0)
+        {
+            t_sum += elapsed_time;
+            t2_sum += elapsed_time * elapsed_time;
+        }
+
+        CHECK(cudaEventDestroy(start));
+        CHECK(cudaEventDestroy(stop));
+    }
+
+    const float t_ave = t_sum / NUM_REPEATS;
+    const float t_err = sqrt(t2_sum / NUM_REPEATS - t_ave * t_ave);
+    printf("Time = %g +- %g ms.\n", t_ave, t_err);
+
+    printf("sum = %f.\n", sum);
+}
+
+real reduce(const real *x, const int N)
+{
+    real sum = 0.0;
+    for (int n = 0; n < N; ++n)
+    {
+        sum += x[n];
+    }
+    return sum;
+}
+
+
