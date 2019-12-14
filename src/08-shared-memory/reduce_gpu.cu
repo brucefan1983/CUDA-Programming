4ed5c477a5bf9e940dd8fa429e5eb4a08e19d6b8
Zheyong Fan <brucenju@gmail.com>
2019-12-15 00:50:41 +0800

    Add files via upload

--- src/08-shared-memory/reduce_gpu.cu
+++ src/08-shared-memory/reduce_gpu.cu
@@ -8,18 +8,15 @@
 #endif
 
 const int NUM_REPEATS = 10;
+const int N = 100000000;
+const int M = sizeof(real) * N;
+const int BLOCK_SIZE = 128;
+const int NUM_ROUNDS = 10;
 
-void timing(real *h_x, real *d_x, const int N, const int method);
-real reduce(real *d_x, const int N, const int method);
-void __global__ reduce_global(real *d_x, real *d_y, const int N);
-void __global__ reduce_shared(real *d_x, real *d_y, const int N);
-void __global__ reduce_dynamic(real *d_x, real *d_y, const int N);
-void __global__ reduce_more(real *d_x, real *d_y, const int N);
+void timing(real *h_x, real *d_x, const int method);
 
 int main(void)
 {
-    const int N = 100000000;
-    const int M = sizeof(real) * N;
     real *h_x = (real *) malloc(M);
     for (int n = 0; n < N; ++n)
     {
@@ -28,110 +25,21 @@ int main(void)
     real *d_x;
     CHECK(cudaMalloc(&d_x, M));
 
-    printf("\nusing global memory only:\n");
-    timing(h_x, d_x, N, 0);
-    printf("\nusing static shared memory:\n");
-    timing(h_x, d_x, N, 1);
-    printf("\nusing dynamic shared memory:\n");
-    timing(h_x, d_x, N, 2);
-    printf("\nusing more parallelism:\n");
-    timing(h_x, d_x, N, 3);
+    printf("\nUsing global memory only:\n");
+    timing(h_x, d_x, 0);
+    printf("\nUsing static shared memory:\n");
+    timing(h_x, d_x, 1);
+    printf("\nUsing dynamic shared memory:\n");
+    timing(h_x, d_x, 2);
+    printf("\nUsing less blocks:\n");
+    timing(h_x, d_x, 3);
 
     free(h_x);
     CHECK(cudaFree(d_x));
     return 0;
 }
 
-void timing(real *h_x, real *d_x, const int N, const int method)
-{
-    real sum = 0;
-    float t_sum = 0;
-    float t2_sum = 0;
-
-    for (int repeat = 0; repeat <= NUM_REPEATS; ++repeat)
-    {
-        const int M = sizeof(real) * N;
-        CHECK(cudaMemcpy(d_x, h_x, M, cudaMemcpyHostToDevice));
-
-        cudaEvent_t start, stop;
-        CHECK(cudaEventCreate(&start));
-        CHECK(cudaEventCreate(&stop));
-        CHECK(cudaEventRecord(start));
-
-        sum = reduce(d_x, N, method);
-
-        CHECK(cudaEventRecord(stop));
-        CHECK(cudaEventSynchronize(stop));
-        float elapsed_time;
-        CHECK(cudaEventElapsedTime(&elapsed_time, start, stop));
-        printf("Time = %g ms.\n", elapsed_time);
-
-        if (repeat > 0)
-        {
-            t_sum += elapsed_time;
-            t2_sum += elapsed_time * elapsed_time;
-        }
-
-        CHECK(cudaEventDestroy(start));
-        CHECK(cudaEventDestroy(stop));
-    }
-
-    const float t_ave = t_sum / NUM_REPEATS;
-    const float t_err = sqrt(t2_sum / NUM_REPEATS - t_ave * t_ave);
-    printf("Time = %g +- %g ms.\n", t_ave, t_err);
-
-    printf("sum = %f.\n", sum);
-}
-
-real reduce(real *d_x, const int N, const int method)
-{
-    const int block_size = 1024;
-    int grid_size = (N + block_size - 1) / block_size;
-    if (method == 3)
-    {
-        const int repeat_size = 10;
-        grid_size = (grid_size + repeat_size - 1) / repeat_size;
-    }
-    const int M = sizeof(real) * grid_size;
-    const int smem = sizeof(real) * block_size;
-    real *d_y;
-    CHECK(cudaMalloc(&d_y, M));
-    real *h_y = (real *) malloc(M);
-
-    switch (method)
-    {
-        case 0:
-            reduce_global<<<grid_size, block_size>>>(d_x, d_y, N);
-            break;
-        case 1:
-            reduce_shared<<<grid_size, block_size>>>(d_x, d_y, N);
-            break;
-        case 2:
-            reduce_dynamic<<<grid_size, block_size, smem>>>(d_x, d_y, N);
-            break;
-        case 3:
-            reduce_more<<<grid_size, block_size, smem>>>(d_x, d_y, N);
-            break;
-        default:
-            printf("Error: wrong method\n");
-            exit(1);
-            break;
-    }
-
-    CHECK(cudaMemcpy(h_y, d_y, M, cudaMemcpyDeviceToHost));
-
-    real result = 0.0;
-    for (int n = 0; n < grid_size; ++n)
-    {
-        result += h_y[n];
-    }
-
-    free(h_y);
-    CHECK(cudaFree(d_y));
-    return result;
-}
-
-void __global__ reduce_global(real *d_x, real *d_y, const int N)
+void __global__ reduce_global(real *d_x, real *d_y)
 {
     const int tid = threadIdx.x;
     real *x = d_x + blockDim.x * blockIdx.x;
@@ -152,7 +60,7 @@ void __global__ reduce_global(real *d_x, real *d_y, const int N)
     }
 }
 
-void __global__ reduce_shared(real *d_x, real *d_y, const int N)
+void __global__ reduce_shared(real *d_x, real *d_y)
 {
     const int tid = threadIdx.x;
     const int bid = blockIdx.x;
@@ -177,8 +85,7 @@ void __global__ reduce_shared(real *d_x, real *d_y, const int N)
     }
 }
 
-
-void __global__ reduce_dynamic(real *d_x, real *d_y, const int N)
+void __global__ reduce_dynamic(real *d_x, real *d_y)
 {
     const int tid = threadIdx.x;
     const int bid = blockIdx.x;
@@ -203,15 +110,14 @@ void __global__ reduce_dynamic(real *d_x, real *d_y, const int N)
     }
 }
 
-void __global__ reduce_more(real *d_x, real *d_y, const int N)
+void __global__ reduce_less(real *d_x, real *d_y)
 {
     const int tid = threadIdx.x;
     const int bid = blockIdx.x;
     extern __shared__ real s_y[];
 
     real y = 0.0;
-    const int stride = blockDim.x * gridDim.x;
-    for (int n = bid * blockDim.x + tid; n < N; n += stride)
+    for (int n = bid * blockDim.x + tid; n < N; n += blockDim.x * gridDim.x)
     {
         y += d_x[n];
     }
@@ -231,6 +137,92 @@ void __global__ reduce_more(real *d_x, real *d_y, const int N)
     {
         d_y[bid] = s_y[0];
     }
+}
+
+real reduce(real *d_x, const int method)
+{
+    int grid_size = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    if (method >= 3)
+    {
+        grid_size = (grid_size + NUM_ROUNDS - 1) / NUM_ROUNDS;
+    }
+    const int ymem = sizeof(real) * grid_size;
+    const int smem = sizeof(real) * BLOCK_SIZE;
+    real *d_y;
+    CHECK(cudaMalloc(&d_y, ymem));
+    real *h_y = (real *) malloc(ymem);
+
+    switch (method)
+    {
+        case 0:
+            reduce_global<<<grid_size, BLOCK_SIZE>>>(d_x, d_y);
+            break;
+        case 1:
+            reduce_shared<<<grid_size, BLOCK_SIZE>>>(d_x, d_y);
+            break;
+        case 2:
+            reduce_dynamic<<<grid_size, BLOCK_SIZE, smem>>>(d_x, d_y);
+            break;
+        case 3:
+            reduce_less<<<grid_size, BLOCK_SIZE, smem>>>(d_x, d_y);
+            break;
+        default:
+            printf("Error: wrong method\n");
+            exit(1);
+            break;
+    }
+
+    CHECK(cudaMemcpy(h_y, d_y, ymem, cudaMemcpyDeviceToHost));
+
+    real result = 0.0;
+    for (int n = 0; n < grid_size; ++n)
+    {
+        result += h_y[n];
+    }
+
+    free(h_y);
+    CHECK(cudaFree(d_y));
+    return result;
+}
+
+void timing(real *h_x, real *d_x, const int method)
+{
+    real sum = 0;
+    float t_sum = 0;
+    float t2_sum = 0;
+
+    for (int repeat = 0; repeat <= NUM_REPEATS; ++repeat)
+    {
+        CHECK(cudaMemcpy(d_x, h_x, M, cudaMemcpyHostToDevice));
+
+        cudaEvent_t start, stop;
+        CHECK(cudaEventCreate(&start));
+        CHECK(cudaEventCreate(&stop));
+        CHECK(cudaEventRecord(start));
+
+        sum = reduce(d_x, method);
+
+        CHECK(cudaEventRecord(stop));
+        CHECK(cudaEventSynchronize(stop));
+        float elapsed_time;
+        CHECK(cudaEventElapsedTime(&elapsed_time, start, stop));
+        printf("Time = %g ms.\n", elapsed_time);
+
+        if (repeat > 0)
+        {
+            t_sum += elapsed_time;
+            t2_sum += elapsed_time * elapsed_time;
+        }
+
+        CHECK(cudaEventDestroy(start));
+        CHECK(cudaEventDestroy(stop));
+    }
+
+    const float t_ave = t_sum / NUM_REPEATS;
+    const float t_err = sqrt(t2_sum / NUM_REPEATS - t_ave * t_ave);
+    printf("Time = %g +- %g ms.\n", t_ave, t_err);
+
+    printf("sum = %f.\n", sum);
 }
 
 
