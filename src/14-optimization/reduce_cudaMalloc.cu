8c813a06e8ea6d874b3a6148955ad2fde5e05962
Zheyong Fan <brucenju@gmail.com>
2019-12-20 07:36:30 +0200

    more optimization

---
+++ src/14-optimization/reduce_cudaMalloc.cu
@@ -0,0 +1,135 @@
+#include "error.cuh"
+#include <stdio.h>
+#include <cooperative_groups.h>
+using namespace cooperative_groups;
+
+#ifdef USE_DP
+    typedef double real;
+#else
+    typedef float real;
+#endif
+
+const int NUM_REPEATS = 10;
+const int N = 100000000;
+const int M = sizeof(real) * N;
+const int BLOCK_SIZE = 128;
+const int NUM_ROUNDS = 10;
+
+void timing(const real *d_x, real *d_y);
+
+int main(void)
+{
+    real *h_x = (real *) malloc(M);
+    for (int n = 0; n < N; ++n)
+    {
+        h_x[n] = 1.23;
+    }
+    real *d_x;
+    CHECK(cudaMalloc(&d_x, M));
+    CHECK(cudaMemcpy(d_x, h_x, M, cudaMemcpyHostToDevice));
+
+    int grid_size = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    grid_size = (grid_size + NUM_ROUNDS - 1) / NUM_ROUNDS;
+    real *d_y;
+    CHECK(cudaMalloc(&d_y, sizeof(real) * grid_size));
+
+    timing(d_x, d_y);
+
+    free(h_x);
+    CHECK(cudaFree(d_x));
+    CHECK(cudaFree(d_y));
+    return 0;
+}
+
+void __global__ reduce_cp(const real *d_x, real *d_y, const int N)
+{
+    const int tid = threadIdx.x;
+    const int bid = blockIdx.x;
+    extern __shared__ real s_y[];
+
+    real y = 0.0;
+    const int stride = blockDim.x * gridDim.x;
+    for (int n = bid * blockDim.x + tid; n < N; n += stride)
+    {
+        y += d_x[n];
+    }
+    s_y[tid] = y;
+    __syncthreads();
+
+    for (int offset = blockDim.x >> 1; offset >= 32; offset >>= 1)
+    {
+        if (tid < offset)
+        {
+            s_y[tid] += s_y[tid + offset];
+        }
+        __syncthreads();
+    }
+
+    y = s_y[tid];
+
+    thread_block_tile<32> g = tiled_partition<32>(this_thread_block());
+    for (int i = g.size() >> 1; i > 0; i >>= 1)
+    {
+        y += g.shfl_down(y, i);
+    }
+
+    if (tid == 0)
+    {
+        d_y[bid] = y;
+    }
+}
+
+real reduce(const real *d_x, real *d_y)
+{
+    int grid_size = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    grid_size = (grid_size + NUM_ROUNDS - 1) / NUM_ROUNDS;
+    const int smem = sizeof(real) * BLOCK_SIZE;
+
+    reduce_cp<<<grid_size, BLOCK_SIZE, smem>>>(d_x, d_y, N);
+    reduce_cp<<<1, 1024, sizeof(real) * 1024>>>(d_y, d_y, grid_size);
+
+    real h_y[1] = {0};
+    CHECK(cudaMemcpy(h_y, d_y, sizeof(real), cudaMemcpyDeviceToHost));
+
+    return h_y[0];
+}
+
+void timing(const real *d_x, real *d_y)
+{
+    real sum = 0;
+    float t_sum = 0;
+    float t2_sum = 0;
+
+    for (int repeat = 0; repeat <= NUM_REPEATS; ++repeat)
+    {
+        cudaEvent_t start, stop;
+        CHECK(cudaEventCreate(&start));
+        CHECK(cudaEventCreate(&stop));
+        CHECK(cudaEventRecord(start));
+
+        sum = reduce(d_x, d_y); 
+
+        CHECK(cudaEventRecord(stop));
+        CHECK(cudaEventSynchronize(stop));
+        float elapsed_time;
+        CHECK(cudaEventElapsedTime(&elapsed_time, start, stop));
+        printf("Time = %g ms.\n", elapsed_time);
+
+        if (repeat > 0)
+        {
+            t_sum += elapsed_time;
+            t2_sum += elapsed_time * elapsed_time;
+        }
+
+        CHECK(cudaEventDestroy(start));
+        CHECK(cudaEventDestroy(stop));
+    }
+
+    const float t_ave = t_sum / NUM_REPEATS;
+    const float t_err = sqrt(t2_sum / NUM_REPEATS - t_ave * t_ave);
+    printf("Time = %g +- %g ms.\n", t_ave, t_err);
+
+    printf("sum = %f.\n", sum);
+}
+
+
