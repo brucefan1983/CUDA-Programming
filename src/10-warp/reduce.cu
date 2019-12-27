974f5a5ffe65689e0eab9e476ab47dfc5208cd5a
Zheyong Fan <brucenju@gmail.com>
2019-12-27 08:14:06 +0200

    using atomic here

--- src/10-warp/reduce.cu
+++ src/10-warp/reduce.cu
@@ -13,7 +13,6 @@ const int NUM_REPEATS = 10;
 const int N = 100000000;
 const int M = sizeof(real) * N;
 const int BLOCK_SIZE = 128;
-const int MAX_THREAD = 1024;
 
 void timing(const real *d_x, const int method);
 
@@ -44,15 +43,9 @@ void __global__ reduce_syncwarp(const real *d_x, real *d_y, const int N)
 {
     const int tid = threadIdx.x;
     const int bid = blockIdx.x;
+    const int n = bid * blockDim.x + tid;
     extern __shared__ real s_y[];
-
-    real y = 0.0;
-    const int stride = blockDim.x * gridDim.x;
-    for (int n = bid * blockDim.x + tid; n < N; n += stride)
-    {
-        y += d_x[n];
-    }
-    s_y[tid] = y;
+    s_y[tid] = (n < N) ? d_x[n] : 0.0;
     __syncthreads();
 
     for (int offset = blockDim.x >> 1; offset >= 32; offset >>= 1)
@@ -75,7 +68,7 @@ void __global__ reduce_syncwarp(const real *d_x, real *d_y, const int N)
 
     if (tid == 0)
     {
-        d_y[bid] = s_y[0];
+        atomicAdd(d_y, s_y[0]);
     }
 }
 
@@ -83,15 +76,9 @@ void __global__ reduce_shfl(const real *d_x, real *d_y, const int N)
 {
     const int tid = threadIdx.x;
     const int bid = blockIdx.x;
+    const int n = bid * blockDim.x + tid;
     extern __shared__ real s_y[];
-
-    real y = 0.0;
-    const int stride = blockDim.x * gridDim.x;
-    for (int n = bid * blockDim.x + tid; n < N; n += stride)
-    {
-        y += d_x[n];
-    }
-    s_y[tid] = y;
+    s_y[tid] = (n < N) ? d_x[n] : 0.0;
     __syncthreads();
 
     for (int offset = blockDim.x >> 1; offset >= 32; offset >>= 1)
@@ -103,7 +90,7 @@ void __global__ reduce_shfl(const real *d_x, real *d_y, const int N)
         __syncthreads();
     }
 
-    y = s_y[tid];
+    real y = s_y[tid];
 
     #define FULL_MASK 0xffffffff
     for (int offset = 16; offset > 0; offset >>= 1)
@@ -113,7 +100,7 @@ void __global__ reduce_shfl(const real *d_x, real *d_y, const int N)
 
     if (tid == 0)
     {
-        d_y[bid] = y;
+        atomicAdd(d_y, y);
     }
 }
 
@@ -121,15 +108,9 @@ void __global__ reduce_cp(const real *d_x, real *d_y, const int N)
 {
     const int tid = threadIdx.x;
     const int bid = blockIdx.x;
+    const int n = bid * blockDim.x + tid;
     extern __shared__ real s_y[];
-
-    real y = 0.0;
-    const int stride = blockDim.x * gridDim.x;
-    for (int n = bid * blockDim.x + tid; n < N; n += stride)
-    {
-        y += d_x[n];
-    }
-    s_y[tid] = y;
+    s_y[tid] = (n < N) ? d_x[n] : 0.0;
     __syncthreads();
 
     for (int offset = blockDim.x >> 1; offset >= 32; offset >>= 1)
@@ -141,7 +122,7 @@ void __global__ reduce_cp(const real *d_x, real *d_y, const int N)
         __syncthreads();
     }
 
-    y = s_y[tid];
+    real y = s_y[tid];
 
     thread_block_tile<32> g = tiled_partition<32>(this_thread_block());
     for (int i = g.size() >> 1; i > 0; i >>= 1)
@@ -151,34 +132,30 @@ void __global__ reduce_cp(const real *d_x, real *d_y, const int N)
 
     if (tid == 0)
     {
-        d_y[bid] = y;
+        atomicAdd(d_y, y);
     }
 }
 
 real reduce(const real *d_x, const int method)
 {
     const int grid_size = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
-    const int ymem = sizeof(real) * grid_size;
-    const int smem1 = sizeof(real) * BLOCK_SIZE;
-    const int smem2 = sizeof(real) * MAX_THREAD;
+    const int smem = sizeof(real) * BLOCK_SIZE;
 
     real h_y[1] = {0};
     real *d_y;
-    CHECK(cudaMalloc(&d_y, ymem));
+    CHECK(cudaMalloc(&d_y, sizeof(real)));
+    CHECK(cudaMemcpy(d_y, h_y, sizeof(real), cudaMemcpyHostToDevice));
 
     switch (method)
     {
         case 0:
-            reduce_syncwarp<<<grid_size, BLOCK_SIZE, smem1>>>(d_x, d_y, N);
-            reduce_syncwarp<<<1, MAX_THREAD, smem2>>>(d_y, d_y, grid_size);
+            reduce_syncwarp<<<grid_size, BLOCK_SIZE, smem>>>(d_x, d_y, N);
             break;
         case 1:
-            reduce_shfl<<<grid_size, BLOCK_SIZE, smem1>>>(d_x, d_y, N);
-            reduce_shfl<<<1, MAX_THREAD, smem2>>>(d_y, d_y, grid_size);
+            reduce_shfl<<<grid_size, BLOCK_SIZE, smem>>>(d_x, d_y, N);
             break;
         case 2:
-            reduce_cp<<<grid_size, BLOCK_SIZE, smem1>>>(d_x, d_y, N);
-            reduce_cp<<<1, MAX_THREAD, smem2>>>(d_y, d_y, grid_size);
+            reduce_cp<<<grid_size, BLOCK_SIZE, smem>>>(d_x, d_y, N);
             break;
         default:
             printf("Wrong method.\n");
