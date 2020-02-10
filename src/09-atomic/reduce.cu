0655dd47f89f98e5c038f4f8c7027375eb56374f
Zheyong Fan <brucenju@gmail.com>
2020-02-10 13:42:44 -0800

    change timing

--- src/09-atomic/reduce.cu
+++ src/09-atomic/reduce.cu
@@ -7,7 +7,7 @@
     typedef float real;
 #endif
 
-const int NUM_REPEATS = 10;
+const int NUM_REPEATS = 100;
 const int N = 100000000;
 const int M = sizeof(real) * N;
 const int BLOCK_SIZE = 128;
@@ -78,8 +78,6 @@ real reduce(const real *d_x)
 void timing(const real *d_x)
 {
     real sum = 0;
-    float t_sum = 0;
-    float t2_sum = 0;
 
     for (int repeat = 0; repeat <= NUM_REPEATS; ++repeat)
     {
@@ -96,19 +94,9 @@ void timing(const real *d_x)
         CHECK(cudaEventElapsedTime(&elapsed_time, start, stop));
         printf("Time = %g ms.\n", elapsed_time);
 
-        if (repeat > 0)
-        {
-            t_sum += elapsed_time;
-            t2_sum += elapsed_time * elapsed_time;
-        }
-
         CHECK(cudaEventDestroy(start));
         CHECK(cudaEventDestroy(stop));
     }
-
-    const float t_ave = t_sum / NUM_REPEATS;
-    const float t_err = sqrt(t2_sum / NUM_REPEATS - t_ave * t_ave);
-    printf("Time = %g +- %g ms.\n", t_ave, t_err);
 
     printf("sum = %f.\n", sum);
 }
