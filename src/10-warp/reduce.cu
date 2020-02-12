b5b085d784dd839df361d7614fbc9d70102954a0
Zheyong Fan <brucenju@gmail.com>
2020-02-11 21:23:16 -0800

    change timing

--- src/10-warp/reduce.cu
+++ src/10-warp/reduce.cu
@@ -9,7 +9,7 @@ using namespace cooperative_groups;
     typedef float real;
 #endif
 
-const int NUM_REPEATS = 10;
+const int NUM_REPEATS = 100;
 const int N = 100000000;
 const int M = sizeof(real) * N;
 const int BLOCK_SIZE = 128;
@@ -171,10 +171,8 @@ real reduce(const real *d_x, const int method)
 void timing(const real *d_x, const int method)
 {
     real sum = 0;
-    float t_sum = 0;
-    float t2_sum = 0;
-
-    for (int repeat = 0; repeat <= NUM_REPEATS; ++repeat)
+    
+    for (int repeat = 0; repeat < NUM_REPEATS; ++repeat)
     {
         cudaEvent_t start, stop;
         CHECK(cudaEventCreate(&start));
@@ -189,19 +187,9 @@ void timing(const real *d_x, const int method)
         CHECK(cudaEventElapsedTime(&elapsed_time, start, stop));
         printf("Time = %g ms.\n", elapsed_time);
 
-        if (repeat > 0)
-        {
-            t_sum += elapsed_time;
-            t2_sum += elapsed_time * elapsed_time;
-        }
-
         CHECK(cudaEventDestroy(start));
         CHECK(cudaEventDestroy(stop));
     }
-
-    const float t_ave = t_sum / NUM_REPEATS;
-    const float t_err = sqrt(t2_sum / NUM_REPEATS - t_ave * t_ave);
-    printf("Time = %g +- %g ms.\n", t_ave, t_err);
 
     printf("sum = %f.\n", sum);
 }
