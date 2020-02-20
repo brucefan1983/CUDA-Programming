19e64ee0a1eacbed0901d086b77cbe36dac68da1
Zheyong Fan <brucenju@gmail.com>
2020-02-20 12:48:46 -0800

    small changes

--- src/12-unified-memory/add.cu
+++ src/12-unified-memory/add.cu
@@ -1,49 +1,58 @@
-#include "error.cuh"
+#include "error.cuh" 
 #include <math.h>
 #include <stdio.h>
-#define EPSILON 1.0e-14
-void __global__ add(double *x, double *y, double *z, int N);
-void check(double *z, int N);
+
+const double EPSILON = 1.0e-15;
+const double a = 1.23;
+const double b = 2.34;
+const double c = 3.57;
+void __global__ add(const double *x, const double *y, double *z);
+void check(const double *z, const int N);
 
 int main(void)
 {
-    int N = 100000000;
-    int M = sizeof(double) * N;
+    const int N = 100000000;
+    const int M = sizeof(double) * N;
     double *x, *y, *z;
-    CHECK(cudaMallocManaged((void**)&x, M))
-    CHECK(cudaMallocManaged((void**)&y, M))
-    CHECK(cudaMallocManaged((void**)&z, M))
+    CHECK(cudaMallocManaged((void **)&x, M));
+    CHECK(cudaMallocManaged((void **)&y, M));
+    CHECK(cudaMallocManaged((void **)&z, M));
+
     for (int n = 0; n < N; ++n)
     {
-        x[n] = 1.0; y[n] = 2.0; z[n] = 0.0;
+        x[n] = a;
+        y[n] = b;
     }
 
-    int block_size = 128;
-    int grid_size = (N - 1) / block_size + 1;
-    add<<<grid_size, block_size>>>(x, y, z, N);
-    
-    CHECK(cudaDeviceSynchronize())
-    
+    const int block_size = 128;
+    const int grid_size = N / block_size;
+    add<<<grid_size, block_size>>>(x, y, z);
+
+    CHECK(cudaDeviceSynchronize());
     check(z, N);
 
-    CHECK(cudaFree(x)) 
-    CHECK(cudaFree(y))
-    CHECK(cudaFree(z))
+    CHECK(cudaFree(x));
+    CHECK(cudaFree(y));
+    CHECK(cudaFree(z));
     return 0;
 }
 
-void __global__ add(double *x, double *y, double *z, int N)
+void __global__ add(const double *x, const double *y, double *z)
 {
-    int n = blockDim.x * blockIdx.x + threadIdx.x;
-    if (n < N) { z[n] = x[n] + y[n]; }
+    const int n = blockDim.x * blockIdx.x + threadIdx.x;
+    z[n] = x[n] + y[n];
 }
 
-void check(double *z, int N)
+void check(const double *z, const int N)
 {
-    int has_error = 0;
+    bool has_error = false;
     for (int n = 0; n < N; ++n)
     {
-        has_error += (fabs(z[n] - 3.0) > EPSILON);
+        if (fabs(z[n] - c) > EPSILON)
+        {
+            has_error = true;
+        }
     }
     printf("%s\n", has_error ? "Has errors" : "No errors");
 }
+
